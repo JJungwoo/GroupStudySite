9b3ba66500bb6998bbd0ba7c683351cf134de298
jungwoo <jungwoo5759@gmail.com>
2020-11-29 18:34:06 +0900

    그룹 스터디 사이트 초기설정

---
+++ src/main/java/com/gss/groupstudysite/GroupstudysiteApplication.java
@@ -0,0 +1,13 @@
+package com.gss.groupstudysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupstudysiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupstudysiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gss/groupstudysite/GroupstudysiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gss.groupstudysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupstudysiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
