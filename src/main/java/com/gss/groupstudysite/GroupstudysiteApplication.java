96c26ad4a9d5f40328442f53708b7fe1c6a8ed6c
jungwoo jo <jungwoo5759@gmail.com>
2020-11-30 22:26:27 +0900

    Develop (#1)
    * 기능 추가: 메일 서비스를 위한 기능 추가
    
    - 메일 기능 관련(메일 서비스, 컨트롤러, DTO) 추가
    - swagger 설정
    
    * 기능 추가: 메일 컨트롤러 주석 제거
    
    * Create README.md

--- src/main/java/com/gss/groupstudysite/GroupstudysiteApplication.java
+++ src/main/java/com/gss/groupstudysite/GroupstudysiteApplication.java
@@ -2,12 +2,20 @@ package com.gss.groupstudysite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GroupstudysiteApplication {
 
+    private static final String PROPERTIES = "spring.config.location=" +
+            "classpath:/application.yml" +
+            ",classpath:/secret.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(GroupstudysiteApplication.class, args);
+        //SpringApplication.run(GroupstudysiteApplication.class, args);
+        new SpringApplicationBuilder(GroupstudysiteApplication.class)
+                .properties(PROPERTIES)
+                .run(args);
     }
 
 }
